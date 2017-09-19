aba5284bef0aa749fadb3a87f624dcc58ad25a99
YANJF123 <2694589781@qq.com>
2017-09-19 15:05:27 +0800

    depend on2

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,40 @@ var gulp = require('gulp');
 /* 公共代码复用 */
 var fileinclude = require('gulp-file-include');
 gulp.task('fileinclude', function () {
-    gulp.src('src/**/*.html')
+    return gulp.src('src/**/*.html')
         .pipe(fileinclude({
             prefix: '@@',
             basepath: '@file'
         }))
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy:libs',function () {
+    return gulp.src('src/libs/**/*')
+        .pipe(gulp.dest('dist/libs'));
+});
+
+var del = require('del');
+
+gulp.task('clean',function () {
+    return del('dist');
+});
+
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+gulp.task('styles', function () {
+    return gulp.src('src/styles/**/*.scss')
+        .pipe(plumber())
+        .pipe(sass.sync({
+            outputStyle: 'nested' // Default: nested Values: nested, expanded, compact, compressed
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist/styles'))
+        .pipe(gulp.dest('./src/styles')); // 仅仅为了提示
 });
 
 /* 时时更新*/
@@ -26,28 +54,21 @@ gulp.task('serve', function() {
         }
     });
     gulp.watch(['src/**/*.html'], ['fileinclude', browserSync.reload]);
-  /*  gulp.watch([paths.stylesFrom], ['styles', browserSync.reload]);
-    gulp.watch([paths.scriptsFrom], ['scripts', browserSync.reload]);*/
+    gulp.watch(['dist/**/*.html'], browserSync.reload);
+    gulp.watch(['src/styles/**/*.scss'],['styles',browserSync.reload]);
 });
 
 
+var runSequence = require('run-sequence');
 
 
-// gulp-clean-css 压缩 css
-exports.cleanCSS = {
-    compatibility: 'ie8',
-    keepSpecialComments: '*'
-};
-
-// gulp-uglify 压缩 js
-exports.uglify = {
-    mangle: {
-        except: ['require', 'exports', 'module', '$']
-    }
-};
-
-
-gulp.task('default', ['serve']);
+gulp.task('default', function () {
+    runSequence('clean',
+        'copy:libs',
+        'fileinclude',
+        'styles',
+        'serve');
+});
 
 
 
