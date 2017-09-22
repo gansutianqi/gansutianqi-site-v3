b49b6ec69b42fca972a2b884d47bb8a03583892f
YANJF123 <2694589781@qq.com>
2017-09-22 18:03:43 +0800

    article

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,36 +3,4 @@
  * Created by Administrator on 2017/9/18.
  */
 
-$('#js-grid-slider-projects').cubeportfolio({
-    layoutMode: 'slider',
-    drag: true,
-    auto: true,
-    autoTimeout: 3000,
-    autoPauseOnHover: true,
-    showNavigation: false,
-    showPagination: false,
-    rewindNav: true,
-    scrollByPage: false,
-    gridAdjustment: 'responsive',
-    mediaQueries: [{
-        width: 1500,
-        cols: 5
-    }, {
-        width: 1100,
-        cols: 4
-    }, {
-        width: 800,
-        cols: 3
-    }, {
-        width: 480,
-        cols: 2
-    }, {
-        width: 320,
-        cols: 1
-    }],
-    gapHorizontal: 0,
-    gapVertical: 5,
-    caption: 'fadeIn',
-    displayType: 'fadeIn',
-    displayTypeSpeed: 100,
-});
+
