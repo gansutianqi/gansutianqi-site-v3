8e58f39cf0018a0726c9a037510ecf5c1418c2e1
buuug7 <youpp@126.com>
2017-10-16 11:48:05 +0800

    fixed

--- themes/bs4/assets/scripts/src/navbar-scroll.js
+++ themes/bs4/assets/scripts/src/navbar-scroll.js
@@ -2,7 +2,7 @@ export default function navbarScroll() {
     window.onscroll = function () {
         let $navbar = $('#navbar-tq');
         let $navbarLink = $navbar.find('.nav-link');
-        if ($navbar.offset().top >= $navbar.height()*2) {
+        if ($navbar.offset().top >= $navbar.height()*1.5) {
             $navbar.addClass('box-shadow bg-white');
             $navbarLink.addClass('nav-link-black');
         } else {
